5fcf8a66ff6dc2a771eb0705ea4776c534dbaa18
Vasundhra272 <queenoftears9091@gmail.com>
2025-06-18 18:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = joblib.load('model.pkl')
 st.markdown("""<h1 style='text-align:center; font-size:40px; color:darkblue;'>🏡 House Price Predictor </h1>
             <p style='text-align:center; font-size:30px; color:darkblue'> Fill the detail below</p>""",unsafe_allow_html=True)
 
-st.markdown("<p style='font-size:20px;'>Enter House Size (sqft)</p>",unsafe_allow_html=True)
+st.markdown("<p style='font-size:20px;'>Enter House Size </p>",unsafe_allow_html=True)
 size = st.number_input("Area in sqft", min_value=500, max_value=5000000, step=100)
 
 st.markdown("<p style='font-size:20px;'>Number of Bedroom</p>",unsafe_allow_html=True)
@@ -24,6 +24,6 @@ age = st.slider("Age of house (years)", 0, 30, 5)
 if st.button("🔍Predict Price"):
     data = [[size, bed, bath, age]]
     prediction = model.predict(data)
-    st.markdown(f"""<h3 style='text-align:center;'>🔘 Estimated Price: ₹{int(prediction[0]):,}</h3>""", unsafe_allow_html=True)
+    st.markdown(f"""<h3 style='text-align:center; color:green;'>🔘 Estimated Price: ₹{int(prediction[0]):,}</h3>""", unsafe_allow_html=True)
 
 
